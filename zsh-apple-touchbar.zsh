71041cea8fcf4764976c2c062ff0d76801b65c0a
Phillip O'Reggio <pnoreggio@gmail.com>
2020-07-17 20:18:31 -0400

    Cleanup variable names

--- zsh-apple-touchbar.zsh
+++ zsh-apple-touchbar.zsh
@@ -9,18 +9,20 @@ function main_view() {
 
     # Number of function keys to use for history (Ex: 12 -> F1 - F12)
     history_keys=12
+
     # How deep to search zhistory for unique commands
     depth=15
 
     # Get latest and remove duplicates/whitespace
-    latest=$(history -$depth | sed 's/^ [0-9][0-9]*\**  *//g' | tail -r)
-    cmds=("${(fu)latest}")
+    latest_commands=$(history -$depth | sed 's/^ [0-9][0-9]*\**  *//g' | tail -r)
+    command_list=("${(fu)latest_commands}")
+
     # Create keys
     hist_keys=($history_keys - 1)
-    num_keys=$(($history_keys>${#cmds[@]} ? ${#cmds[@]} : $history_keys))
-    for (( i = 0 ; i < $num_keys ; i++ ))
-    do
-        create_key $i "${cmds[$i]}" "${cmds[$i]}" '-s' 2> /dev/null
+    num_keys=$(($history_keys>${#command_list[@]} ? ${#command_list[@]} : $history_keys))
+
+    for (( i = 0 ; i < $num_keys ; i++ )); do
+        create_key $i "${command_list[$i]}" "${command_list[$i]}" '-s' 2> /dev/null
     done
 
     # TODO:  See TODO.md
