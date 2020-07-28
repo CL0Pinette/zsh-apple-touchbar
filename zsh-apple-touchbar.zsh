ca143cc44e184e01529b7f246248c4b525944c5a
Phillip O'Reggio <pnoreggio@gmail.com>
2020-07-27 20:00:12 -0400

    Make constant commands button

--- zsh-apple-touchbar.zsh
+++ zsh-apple-touchbar.zsh
@@ -1,11 +1,11 @@
 source ${0:A:h}/functions.zsh
 
-set_state 'main'
+set_state 'command_history'
 
-function main_view() {
+function history_view() {
 	remove_and_unbind_keys
 
-	set_state 'main'
+	set_state 'command_history'
 
     # Number of function keys to use for history (Ex: 12 -> F1 - F12)
     history_keys=12
@@ -13,24 +13,49 @@ function main_view() {
     # How deep to search zhistory for unique commands
     depth=15
 
+    # How many keys are reserved for other purposes than history
+    reserved_keys=1
+
     # Get latest and remove duplicates/whitespace
     latest_commands=$(history -$depth | sed 's/^ [0-9][0-9]*\**  *//g' | tail -r)
     command_list=("${(fu)latest_commands}")
 
-    # Create keys
-    hist_keys=($history_keys - 1)
-    num_keys=$(($history_keys>${#command_list[@]} ? ${#command_list[@]} : $history_keys))
+    # To common commands
+    create_key 1 '📕' 'commands_view'
 
-    for (( i = 0 ; i < $num_keys ; i++ )); do
-        create_key $i "${command_list[$i]}" "${command_list[$i]}" '-s' 2> /dev/null
+    # Create keys
+    num_keys_to_display=$(( $history_keys > ${#command_list[@]} ? ${#command_list[@]} : $history_keys))
+    num_keys_to_display=$(( $num_keys_to_display + $reserved_keys ))
+
+    for (( i = $reserved_keys + 1 ; i < $num_keys_to_display ; i++ )); do
+        list_index=$(($i - $reserved_keys))
+        create_key "$i" "${command_list[$list_index]}" "${command_list[$list_index]}" '-s' 2> /dev/null
     done
+
 }
 
-zle -N main_view
+function configs_view() {
+    remove_and_unbind_keys
+
+    set_state 'configs'
+
+    create_key 1 '📖' 'history_view'
+
+    # Commands
+    create_key 2 'emacs' 'emacs &' '-s'
+    create_key 3 'vi ~/.vimrc' 'vi ~/.vimrc' '-s'
+    create_key 4 'vi ~/.zshrc' 'vi ~/.zshrc' '-s'
+    create_key 5 'vi colors' 'vi ~/.vim/colors/' '-s'
+}
+
+
+zle -N history_view
+zle -N commands_view
 
 precmd_apple_touchbar() {
 	case $state in
-		main) main_view ;;
+        command_history)    history_view ;;
+		configs)            configs_view ;;
 	esac
 }
 
