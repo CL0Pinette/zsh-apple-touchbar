2e92a7ceef006553f36b936a5d8f1724770f5515
Phillip O'Reggio <pnoreggio@gmail.com>
2020-08-16 01:52:52 -0400

    Start computer stats touchbar item

---
+++ compStats/computer.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# Like top on statusline
+
+# CPU usage: top -l $num | grep "CPU usage"
+# CPU temp: sudo powermetrics --samplers smc
+#
+# GPU usage: ???
+# GPU temp: sudo powermetrics --samplers smc
+#
+# Mem usage: vm_stats + awk script
+# Swap Mem Usage:
+#
+# Have to update all perdiocally every n seconds
